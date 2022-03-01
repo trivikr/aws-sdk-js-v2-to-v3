37eab66d0e314e689b00d8596ae55bca6e0b367e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-03-01 10:26:49 -0800

    Add basic test using jest (#10)

---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,4 @@
+// @ts-ignore Could not find a declaration file for module
+import { defineTest } from "jscodeshift/dist/testUtils";
+
+defineTest(__dirname, "./index", null, "basic", { parser: "ts" });
