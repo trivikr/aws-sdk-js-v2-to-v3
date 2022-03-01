eb2108450a5d3212d327d511cb206af68ad407dd
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-01 14:57:06 -0800

    Update basic input/output test

--- src/__testfixtures__/basic.input.ts
+++ src/__testfixtures__/basic.input.ts
@@ -1,1 +1,5 @@
-const foo = 123;
+import AWS from "aws-sdk";
+
+const region = "us-west-2";
+const client = new AWS.DynamoDB({ region });
+const response = await client.listTables({}).promise();
--- src/__testfixtures__/basic.output.ts
+++ src/__testfixtures__/basic.output.ts
@@ -1,1 +1,5 @@
-const foo = 123;
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+
+const region = "us-west-2";
+const client = new DynamoDB({ region });
+const response = await client.listTables({});
