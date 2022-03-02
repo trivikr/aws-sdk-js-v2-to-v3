73c6e07a7449c07f2c0c3310128b83f42fbe0eab
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-02 00:59:41 -0800

    chore: yarn prettier --write

--- src/utils/getClientName.ts
+++ src/utils/getClientName.ts
@@ -3,7 +3,5 @@ import { CLIENT_NAMES_MAP } from "./config";
 // Returns v3 client name for the provided v2 client name.
 export const getClientName = (clientName: string) => {
   if (clientName in CLIENT_NAMES_MAP) return CLIENT_NAMES_MAP[clientName];
-  throw new Error(
-    `Client '${clientName}' is either deprecated or newly added.`
-  );
+  throw new Error(`Client '${clientName}' is either deprecated or newly added.`);
 };
