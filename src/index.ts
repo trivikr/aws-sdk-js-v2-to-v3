d2ae2ffd995059b14d73f517b9324fff4dcd83b0
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-02 01:22:21 -0800

    Remove redundant newline

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,6 @@ export default function transformer(file: FileInfo, api: API) {
       for (const v2ClientName of v2ClientNames) {
         const v3ClientName = getClientName(v2ClientName);
         const v3PackageName = getClientPackageName(v2ClientName);
-
         addV3ClientImport(j, source, { v3ClientName, v3PackageName });
         replaceClientCreation(j, source, { importObj, v2ClientName, v3ClientName });
       }
