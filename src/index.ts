236e5d6966b6c7fb3c686a7f7f4b14c24c292869
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-01 21:39:36 -0800

    chore: add package import for client

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { API, FileInfo, NewExpression } from "jscodeshift";
 import findImports from "jscodeshift-find-imports";
+import { getClientName } from "./getClientName";
+import { getClientPackageName } from "./getClientPackageName";
 
 export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift;
@@ -9,7 +11,7 @@ export default function transformer(file: FileInfo, api: API) {
   const imports = findImports(root, statement`import AWS from 'aws-sdk'`);
   for (const importObj of Object.values(imports)) {
     if (importObj.type === "Identifier") {
-      const clientNames = root
+      const v2ClientNames = root
         .find(j.NewExpression, {
           callee: {
             object: { type: "Identifier", name: importObj.name },
@@ -22,6 +24,26 @@ export default function transformer(file: FileInfo, api: API) {
             // @ts-ignore
             newExpression.callee.property.name
         );
+
+      for (const v2ClientName of v2ClientNames) {
+        const v3ClientName = getClientName(v2ClientName);
+        const v3PackageName = getClientPackageName(v2ClientName);
+
+        // Add v3 client import.
+        root
+          .find(j.ImportDeclaration)
+          .filter(path => path.value.source.value === "aws-sdk")
+          .insertAfter(
+            j.importDeclaration(
+              [j.importSpecifier(j.identifier(v3ClientName))],
+              j.stringLiteral(v3PackageName)
+            )
+          );
+
+        // Replace v2 client creation with v3 client creation.
+
+        // Replace v2 client API calls with v3 client API calls.
+      }
     }
   }
 
