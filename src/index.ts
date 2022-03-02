eb8f60f6ad05a11a03e1248e2cdfb5fdfacd9f49
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-01 17:08:49 -0800

    Get all client names from SDK v2

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-import { API, FileInfo } from "jscodeshift";
+import { API, FileInfo, NewExpression } from "jscodeshift";
+import findImports from "jscodeshift-find-imports";
 
 export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift;
+  const { statement } = j.template;
   const root = j(file.source);
 
-  // transform `root` here
+  const imports = findImports(root, statement`import AWS from 'aws-sdk'`);
+  for (const importObj of Object.values(imports)) {
+    if (importObj.type === "Identifier") {
+      const clientNames = root
+        .find(j.NewExpression, {
+          callee: {
+            object: { type: "Identifier", name: importObj.name },
+            property: { type: "Identifier" }
+          }
+        })
+        .nodes()
+        .map(
+          newExpression =>
+            // @ts-ignore
+            newExpression.callee.property.name
+        );
+    }
+  }
 
   return root.toSource();
 }
