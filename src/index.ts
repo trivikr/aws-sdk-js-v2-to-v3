9ebdd15a0daf1db2d75278bd1f8c1150e66d19cf
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-02 00:31:43 -0800

    Revert "Introduce unprettified file"
    This reverts commit 12214d03307855e81dc5557fa9a866273b7b272b.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { API, FileInfo } from "jscodeshift";
 
 export default function transformer(file: FileInfo, api: API) {
-  const j = api
-    .jscodeshift;
+  const j = api.jscodeshift;
   const root = j(file.source);
 
   // transform `root` here
