21bff410fd583b1199d9508e1b41960fc5ab63ae
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-01 23:32:19 -0800

    Revert "Introduce lint error to check CI failure"
    This reverts commit 0ee0e47b32b52b70cbe02fac52fff2e5906029af.

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift;
   const root = j(file.source);
 
-  let a = 0;
-  if ((a = 1)) {
-    console.log("test");
-  }
   // transform `root` here
 
   return root.toSource();
