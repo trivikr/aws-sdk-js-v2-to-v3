0ee0e47b32b52b70cbe02fac52fff2e5906029af
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-01 23:29:58 -0800

    Introduce lint error to check CI failure

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift;
   const root = j(file.source);
 
+  let a = 0;
+  if ((a = 1)) {
+    console.log("test");
+  }
   // transform `root` here
 
   return root.toSource();
