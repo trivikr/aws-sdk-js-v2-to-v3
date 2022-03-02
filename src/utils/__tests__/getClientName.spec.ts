f7fff3ae6438c4cd3ea86baf09c87d6c425348f1
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-03-02 01:00:46 -0800

    Add Prettier config with printWidth=120 (#48)

--- src/utils/__tests__/getClientName.spec.ts
+++ src/utils/__tests__/getClientName.spec.ts
@@ -2,36 +2,19 @@ import { CLIENT_NAMES_MAP } from "../config";
 import { getClientName } from "../getClientName";
 
 describe(getClientName.name, () => {
-  it.each(Object.entries(CLIENT_NAMES_MAP))(
-    "getClientName('%s') === '%s'",
-    (input, output) => {
-      expect(getClientName(input)).toBe(output);
-    }
-  );
+  it.each(Object.entries(CLIENT_NAMES_MAP))("getClientName('%s') === '%s'", (input, output) => {
+    expect(getClientName(input)).toBe(output);
+  });
 
-  it.each(["ImportExport", "MobileAnalytics", "SimpleDB"])(
-    "throws for deprecated client '%s'",
-    (deprecatedClient) => {
-      expect(() => {
-        getClientName(deprecatedClient);
-      }).toThrow(
-        new Error(
-          `Client '${deprecatedClient}' is either deprecated or newly added.`
-        )
-      );
-    }
-  );
+  it.each(["ImportExport", "MobileAnalytics", "SimpleDB"])("throws for deprecated client '%s'", (deprecatedClient) => {
+    expect(() => {
+      getClientName(deprecatedClient);
+    }).toThrow(new Error(`Client '${deprecatedClient}' is either deprecated or newly added.`));
+  });
 
-  it.each(["UNDEFINED", "NULL", "UNKNOWN"])(
-    "throws for unknown client '%s'",
-    (unknownClient) => {
-      expect(() => {
-        getClientName(unknownClient);
-      }).toThrow(
-        new Error(
-          `Client '${unknownClient}' is either deprecated or newly added.`
-        )
-      );
-    }
-  );
+  it.each(["UNDEFINED", "NULL", "UNKNOWN"])("throws for unknown client '%s'", (unknownClient) => {
+    expect(() => {
+      getClientName(unknownClient);
+    }).toThrow(new Error(`Client '${unknownClient}' is either deprecated or newly added.`));
+  });
 });
--- src/utils/getClientPackageName.ts
+++ src/utils/getClientPackageName.ts
@@ -2,9 +2,6 @@ import { CLIENT_PACKAGE_NAMES_MAP } from "./config";
 
 // Returns v3 client package name for the provided v2 client name.
 export const getClientPackageName = (clientName: string) => {
-  if (clientName in CLIENT_PACKAGE_NAMES_MAP)
-    return `@aws-sdk/${CLIENT_PACKAGE_NAMES_MAP[clientName]}`;
-  throw new Error(
-    `Client '${clientName}' is either deprecated or newly added.`
-  );
+  if (clientName in CLIENT_PACKAGE_NAMES_MAP) return `@aws-sdk/${CLIENT_PACKAGE_NAMES_MAP[clientName]}`;
+  throw new Error(`Client '${clientName}' is either deprecated or newly added.`);
 };
