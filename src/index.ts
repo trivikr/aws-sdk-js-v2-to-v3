970c78143045d1b13eeecac35805b84295f5a7c2
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-02 00:20:58 -0800

    Revert "Introduce fixable lint issue"
    This reverts commit fea13aaf69680ac73a9056c0fd4b8f248a54ac29.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { FileInfo, API } from "jscodeshift";
+import { API, FileInfo } from "jscodeshift";
 
 export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift;
