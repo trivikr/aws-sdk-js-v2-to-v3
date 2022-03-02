960e65a1ac32976863992e19cc1315b52725b87b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-03-02 01:04:33 -0800

    Set prettier printWidth=100 (#49)

--- src/utils/__tests__/getClientPackageName.spec.ts
+++ src/utils/__tests__/getClientPackageName.spec.ts
@@ -2,15 +2,21 @@ import { CLIENT_PACKAGE_NAMES_MAP } from "../config";
 import { getClientPackageName } from "../getClientPackageName";
 
 describe(getClientPackageName.name, () => {
-  it.each(Object.entries(CLIENT_PACKAGE_NAMES_MAP))("getClientName('%s') === '%s'", (input, output) => {
-    expect(getClientPackageName(input)).toBe(`@aws-sdk/${output}`);
-  });
+  it.each(Object.entries(CLIENT_PACKAGE_NAMES_MAP))(
+    "getClientName('%s') === '%s'",
+    (input, output) => {
+      expect(getClientPackageName(input)).toBe(`@aws-sdk/${output}`);
+    }
+  );
 
-  it.each(["ImportExport", "MobileAnalytics", "SimpleDB"])("throws for deprecated client '%s'", (deprecatedClient) => {
-    expect(() => {
-      getClientPackageName(deprecatedClient);
-    }).toThrow(new Error(`Client '${deprecatedClient}' is either deprecated or newly added.`));
-  });
+  it.each(["ImportExport", "MobileAnalytics", "SimpleDB"])(
+    "throws for deprecated client '%s'",
+    (deprecatedClient) => {
+      expect(() => {
+        getClientPackageName(deprecatedClient);
+      }).toThrow(new Error(`Client '${deprecatedClient}' is either deprecated or newly added.`));
+    }
+  );
 
   it.each(["UNDEFINED", "NULL", "UNKNOWN"])("throws for unknown client '%s'", (unknownClient) => {
     expect(() => {
