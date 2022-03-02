3fb67baee154d17db5b6847ae381a745e2eeaf35
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-03-01 23:34:24 -0800

    Enable ESLint with recommended configuration (#41)

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,4 +1,4 @@
-// @ts-ignore Could not find a declaration file for module
+// @ts-expects-error Could not find a declaration file for module
 import { defineTest } from "jscodeshift/dist/testUtils";
 
 defineTest(__dirname, "./index", null, "basic", { parser: "ts" });
