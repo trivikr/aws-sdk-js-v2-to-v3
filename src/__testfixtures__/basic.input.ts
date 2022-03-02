e8be28e892952731bc4b7175028f1e31a4ab49b0
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-01 21:46:15 -0800

    Update basic test case to use callbacks

--- src/__testfixtures__/basic.input.ts
+++ src/__testfixtures__/basic.input.ts
@@ -2,4 +2,7 @@ import AWS from "aws-sdk";
 
 const region = "us-west-2";
 const client = new AWS.DynamoDB({ region });
-const response = await client.listTables({}).promise();
+client.listTables({}, function(err, data) {
+  if (err) console.log(err, err.stack);
+  else console.log(data);
+});
